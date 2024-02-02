323ad4a650686ff6940c608eb9c516c6fda7c165
yeeunrim <1515eun@daum.net>
2024-02-02 16:41:34 +0900

    upload20240202-3

--- Code/3work/src/main/java/com/work/community/config/SecurityConfig.java
+++ Code/3work/src/main/java/com/work/community/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.work.community.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,12 +9,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.authorizeHttpRequests(authorize -> authorize
-					.requestMatchers("/", "/**", "/css/**", "/js/**", "/images/**").permitAll());
-		return http.build();
-	}
-
+   
+   //@Bean : 프로젝트에서 관리가 안되는 클래스를 빈으로 사용할때 필요
+   //pom.xml에서 security 추가해도 별도의 로그인 없이 접속 가능
+   @Bean
+   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+      
+      http
+         .authorizeHttpRequests(authorize -> authorize
+               .requestMatchers("/**", "/css/**", "/js/**").permitAll()
+               );
+               
+      return http.build();
+   }
+   
 }
---
+++ Code/3work/src/main/java/com/work/community/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.work.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/member/join_wel")
+	public String join_wel() {
+		return "member/join_wel";
+	}
+	
+	@GetMapping("/member/id_search")
+	public String id_search() {
+		return "member/id_search";
+	}
+	
+	@GetMapping("/member/id_result")
+	public String id_result() {
+		return "member/id_result";
+	}
+	
+	@GetMapping("/member/pw_search")
+	public String pw_search() {
+		return "member/pw_search";
+	}
+}
